781bd0e4b4eb7efe178b0cceb12bee6420e9a858
Android Studio <devandroid@ort>
2026-05-11 10:25:10 -0300

    entities

---
+++ src/entities/calificacion.js
@@ -0,0 +1,11 @@
+export default class Calificacion {
+    constructor(id_alumno, nombre_alumno, apellido_alumno, id_materia, nombre_materia, nota, fecha) {
+        this.id_alumno = id_alumno;
+        this.nombre_alumno = nombre_alumno;
+        this.apellido_alumno = apellido_alumno;
+        this.id_materia = id_materia;
+        this.nombre_materia = nombre_materia;
+        this.nota = nota;
+        this.fecha = fecha;
+    }
+}
